e2f35f14c8d7fa8252cac0758f559bf34fe9fb00
MKrsic <krsicmatija@gmail.com>
2016-03-24 10:18:26 +0100

    Sređeni kontroleri
    Sređeni kontroleri za SZC i SZE grafove

--- src/tranzpojacnew/Resources/SZE/Shema/SzeShemaFXMLController.java
+++ src/tranzpojacnew/Resources/SZE/Shema/SzeShemaFXMLController.java
@@ -5,18 +5,9 @@
  */
 package tranzpojacnew.Resources.SZE.Shema;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -24,8 +15,7 @@ import javafx.stage.Stage;
  * @author Matija
  */
 public class SzeShemaFXMLController implements Initializable {
-    @FXML
-    private Button Return1;
+
 
     /**
      * Initializes the controller class.
@@ -35,14 +25,4 @@ public class SzeShemaFXMLController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void Return(ActionEvent event) throws IOException{
-         Parent home_page_parent = FXMLLoader.load(getClass().getResource("/tranzpojacnew/MainMenuFXML.fxml"));
-            Scene home_page_scene = new Scene(home_page_parent);
-            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        
-        app_stage.setScene(home_page_scene);
-        app_stage.show();
-    }
-    
 }
